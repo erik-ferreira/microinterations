2cc97cd786da7072447a0b3cdf974b9bb218e93b
erik-ferreira <erikdesouzagf@gmail.com>
2023-02-16 13:53:39 -0300

    animation with rotate

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-import { LongPress } from "./src/components/LongPress";
+import { Rotation } from "./src/components/Rotation";
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <LongPress />
+      <Rotation />
     </GestureHandlerRootView>
   );
 }
---
+++ src/components/Rotation/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  box: {
+    width: 200,
+    height: 200,
+    borderRadius: 12,
+    backgroundColor: "#7159c1",
+  },
+});
