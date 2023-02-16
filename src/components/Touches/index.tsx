bfedb9a589b02b30a43a1f0ff95390f38eee7dae
erik-ferreira <erikdesouzagf@gmail.com>
2023-02-16 00:24:10 -0300

    add reanimated and gesture handler

---
+++ src/components/Touches/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+import { styles } from "./styles";
+
+export function Touches() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.box} />
+    </View>
+  );
+}
---
+++ src/components/Touches/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: "#7159c1",
+    borderRadius: 12,
+  },
+});
